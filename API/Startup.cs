02befb612a371996826686bbf56cbde282880484
unknown <sauledezma@gmail.com>
2020-11-28 16:00:02 -0800

    Creating generic repo interface

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,6 +49,7 @@ namespace API
             //AddScope(repository to be injected, instance of the concrete class)
             //This allows the ProductRepository to be injectable
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
